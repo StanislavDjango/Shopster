d5ccd9ec3a00e513ba414067b00a0cd10aa73b5c
Stanyslav <nitro04719@gmail.com>
2025-11-05 22:51:19 +0100

    test: avoid manifest staticfiles in smoke tests

---
+++ backend/tests/conftest.py
@@ -0,0 +1,8 @@
+﻿import pytest
+
+
+@pytest.fixture(autouse=True)
+def configure_static_storage(settings):
+    settings.STATICFILES_STORAGE = (
+        "django.contrib.staticfiles.storage.StaticFilesStorage"
+    )
